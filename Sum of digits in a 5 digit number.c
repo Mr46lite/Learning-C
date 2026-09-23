7f3e9b45b6f625ace945dcca752e89ee0c1a63ec
Nandyala Krishna Tej <krishnatejnandyala0@gmail.com>
2026-09-23 15:42:07 +0530

    Refactor digit sum calculation in C program
    Changed the input entry case, from 5 digits to any number is digits

--- Sum of digits in a 5 digit number.c
+++ Sum of digits in a 5 digit number.c
@@ -1,16 +1,14 @@
 #include<stdio.h>
-#include<stdlib.h>
-
-int main()
-{
-int n,fst_dgt,secnd_dgt,thrd_dgt,foth_dgt,fifth_dgt,result;
+int main(){
+int n,sum=0,d,oldn;
+printf("enter number:");
 scanf("%d",&n);
-fst_dgt=n%10;
-secnd_dgt=(n%100-n%10)/10;
-thrd_dgt=(n%1000-n%100)/100;
-foth_dgt=(n%10000-n%1000)/1000;
-fifth_dgt=(n%100000-n%10000)/10000;
-result=fst_dgt+secnd_dgt+thrd_dgt+foth_dgt+fifth_dgt;
-printf("%d",result);
-    return 0;
+oldn=n;
+while(n>0){
+d=n%10;
+sum+=d;
+n=n/10;
+}
+printf("sum of all digits in %d = %d\n",oldn,sum);
+return 0;
 }
